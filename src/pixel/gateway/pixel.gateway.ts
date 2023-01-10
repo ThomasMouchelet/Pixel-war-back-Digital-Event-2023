f1c8766411ce2dde92c0d568ee94cee0d880adfe
Thomas Mouchelet <mouchelet.thomas@gmail.com>
2023-01-10 18:56:43 +0100

    pre-prod clever-cloud

--- src/pixel/gateway/pixel.gateway.ts
+++ src/pixel/gateway/pixel.gateway.ts
@@ -24,26 +24,56 @@ export class PixelGateway {
     private readonly userService: UserService,
   ) {}
 
-  @SubscribeMessage('createPixel')
-  async handleCreatePixel(socket: Socket, payload: PixelDto) {
-    try {
-      const pixel = await this.pixelService.create(payload);
-      const updateUserScore = await this.userService.updateScore(pixel.user.id);
-      console.log('updateUserScore => ', updateUserScore);
-
-      const pixels = await this.pixelService.findAll();
-      const lastUsers = await this.pixelService.findLastTwentyUser();
-      const payloadResponse = {
-        pixels: pixels,
-        lastUsers: lastUsers,
-      };
-      // console.log("payloadResponse => ", payloadResponse);
-      await this.server.emit('responsePixel', payloadResponse);
-    } catch (error) {
-      console.log('error => ', error.message);
-      this.server.emit('createdPixel', {
-        error: new ForbiddenException(error.message),
-      });
-    }
+  async handleConnection(socket: Socket){
+    console.log('Client connected');
+    // Lorsque le user se connecte, on récupère son token
+    // onst decodedToken = await this.authService.verifyJwt(socket.handshake.headers.authorization);
+    // const user = await this.userService.findOne(decodedToken.id);
+    // socket.data.user = user;
+    const pixels = await this.pixelService.findAll();
+    this.server.emit('newPixel', pixels);
   }
+
+  async handleDisconnect(socket: Socket) {
+    console.log('Client disconnected');
+  }
+
+  private disconnect(socket: Socket) {
+    console.log('Client disconnected');
+  }
+
+  @SubscribeMessage('newPixel')
+  async handleNewPixel(socket: Socket, payload: any) {
+    // Récupérer le user dans le socker
+    // const user = socket.data.user;
+    console.log('newPixel', payload);
+    const pixel = await this.pixelService.create(payload);
+    console.log('handleNewPixel create', pixel);
+
+    const pixels = await this.pixelService.findAll();
+    this.server.emit('newPixel', pixels);
+  }
+
+  // @SubscribeMessage('createPixel')
+  // async handleCreatePixel(socket: Socket, payload: PixelDto) {
+  //   try {
+  //     const pixel = await this.pixelService.create(payload);
+  //     const updateUserScore = await this.userService.updateScore(pixel.user.id);
+  //     console.log('updateUserScore => ', updateUserScore);
+
+  //     const pixels = await this.pixelService.findAll();
+  //     const lastUsers = await this.pixelService.findLastTwentyUser();
+  //     const payloadResponse = {
+  //       pixels: pixels,
+  //       lastUsers: lastUsers,
+  //     };
+  //     // console.log("payloadResponse => ", payloadResponse);
+  //     await this.server.emit('responsePixel', payloadResponse);
+  //   } catch (error) {
+  //     console.log('error => ', error.message);
+  //     this.server.emit('createdPixel', {
+  //       error: new ForbiddenException(error.message),
+  //     });
+  //   }
+  // }
 }
